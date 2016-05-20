8a824991f4e6c1fc78789fca0de844880c3c1481
marco1977 <fare.marco@gmail.com>
2016-05-20 10:09:36 +0200

    add dependence angular-szn-autocomplete into module

---
+++ src/app/components/themovieDB/themovieDB.service.js
@@ -0,0 +1,37 @@
+(function() {
+    'use strict';
+    
+    angular
+        .module('massiveInteract2016')
+        .service('themovieDBSrv', themovieDB);
+
+    /** @ngInject */
+    function themovieDB($q) {
+        var data = [ ];
+        this.asyncSearch = asyncSearch;
+
+        function asyncSearch(name) {
+            var deferred = $q.defer();
+            
+            setTimeout(function() {
+                deferred.notify('starting query ' + name + '.');
+        
+                function successCB(data) {
+                    var dataObj = JSON.parse(data);
+                    
+                    console.log("success");
+                    console.log(data);
+                    deferred.resolve(dataObj);
+                };
+                
+                function errorCB() {
+                    deferred.reject();
+                };
+                
+                return theMovieDb.search.getMovie({"query": encodeURI(name)}, successCB, errorCB);
+            }, 1000);
+
+            return deferred.promise;
+        };
+    }
+})();
