dda9f0712ec6aaf03c6b66376a2671f56e4fe934
marco1977 <fare.marco@gmail.com>
2016-05-20 09:20:24 +0200

    first commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('massiveinteracteDemo')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
